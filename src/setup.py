69d2a95d690e8936e4decbe0e1a516d3f3c102f2
Melo Ortega <melo-ortega@adabaed.com>
2023-03-26 03:15:26 +0100

    chore: folder structure

---
+++ src/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tomd',
+    version='0.1',
+    author='adabaed',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'click',
+        'pdfminer.six',
+        'python-docx'
+    ],
+    entry_points='''
+        [console_scripts]
+        tomd=main:tomd
+    '''
+)
